713e5498d796347697f3d6b9ddc41d43d2aa480e
csiggydev <csegalas@gmail.com>
2020-05-04 19:31:43 -0700

    Initial commit

---
+++ Disable_WindowsFirewall.ps1
@@ -0,0 +1,26 @@
+# Script to check windows firewall status and to disable if enabled
+#  version 1.0
+
+function Disable-WindowsFirewall
+{ 
+    # Check if the firewall enabled status is true on any profile, if so, set to false
+
+    $firewallstatus = Get-NetFirewallProfile | Select-Object Name,Enabled 
+    
+    if ($firewallstatus.Enabled -eq 'False') {
+        Write-Host "Windows Firewall is disabled. No action required."
+    }
+    elseif ($firewallstatus.Enabled -eq 'True') {
+        Write-Host "Windows firewall is enabled. Disabling ..."   
+    }
+        try {
+            (Set-NetFirewallProfile -All -Enabled False)
+    }
+        catch [System.IO.IOException] {
+            Write-Host "An error occurred:"
+            Write-Host $_
+   }
+}
+Disable-WindowsFirewall
+
+$verify = Get-NetFirewallProfile | Select-Object Name,Enabled | Write-Host
